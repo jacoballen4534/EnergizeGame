8827dfe4cce44df35896d940101b928773940d43
jacob <jacoballen4534@gmail.com>
2019-04-02 09:05:56 +1300

    Adding parser to new layout

--- src/Controllers/highScoreController.java
+++ src/Controllers/highScoreController.java
@@ -5,19 +5,65 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Pair;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 
 public class highScoreController implements Initializable {
-    @FXML public AnchorPane highScorePane;
+    @FXML
+    public AnchorPane highScorePane;
+    private Label Person1;
+    private Label Score1;
+    private Label Person2;
+    private Label Score2;
+    private Label Person3;
+    private Label Score3;
+    private Label Person4;
+    private Label Score4;
+    private Label Person5;
+    private Label Score5;
+
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        try {
+            File file = new File(highScoreController.class.getClassLoader().getResource("highScores.txt").getPath());
+            Scanner scanner = new Scanner(file);
+            ArrayList<Pair<String, Integer>> scores = new ArrayList<>();
+
+            while(scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (!line.startsWith("//")){ //Ignore comments
+                    String[] tokens = line.split("-");
+                    Pair<String, Integer> score = new Pair<>(tokens[0], Integer.parseInt(tokens[1]));
+                    scores.add(score);
+                }
+            }
+
+            scores.forEach((n) -> System.out.println(n));
+
+            Person1.setText("Hi");
+            Score1.setText(scores.get(0).getKey());
+            Score2.setText(scores.get(0).getValue().toString());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
+
+
+
+
 
     public void /*some list type*/loadHighScores(String filepath){
         //Figure out how file i/o in Java
