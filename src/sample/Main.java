7dbcebc8e8621e9baee3f389c52bb542c4c45b8e
Matteas-Eden <mrmatteden@gmail.com>
2019-04-04 12:46:44 +1300

    Updated stylesheet Added support for a custom font (Beon)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.scene.text.Font;
 
 import java.awt.*;
 import java.io.File;
@@ -14,20 +15,27 @@ public class Main extends Application {
     private static Stage stage;
 
     public static void main(String[] args) {
-//        java.net.URL url = ClassLoader.getSystemResource("/home/matteas/Pictures/lyoko_logo.png");
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
+        //Attempts to load a custom font
+        File beon = new File("resources/fonts/beon.otf");
+        Font.loadFont(
+                getClass().getResourceAsStream("file:///" + beon.getAbsolutePath().replace("\\","/")),
+                10
+        );
+
         stage = primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("../fxmls/mainMenu.fxml"));
         primaryStage.setTitle("Main Menu");
 
-
-        File f = new File("resources/css/Viper.css");
+        //Loads a global stylesheet
+        File styleSheet = new File("resources/css/globalStyle.css");
         Scene scene = new Scene(root);
-        scene.getStylesheets().add("file:///" + f.getAbsolutePath().replace("\\","/"));
+        scene.getStylesheets().add("file:///" + styleSheet.getAbsolutePath().replace("\\","/"));
 
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
