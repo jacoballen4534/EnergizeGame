00e0da7822a2154610f21ee4c44506b729d81d27
Jacob <jacoballen4534@gmail.com>
2019-04-08 12:29:49 +1200

    Add surgestions

--- src/model/Camera.java
+++ src/model/Camera.java
@@ -10,6 +10,7 @@ public class Camera {
     }
 
     public void tick(GameObject gameObject, double windowWidth, double windowHeight, int levelWidth, int levelHeight){
+        //TODO: Can use this to pan towards the boss/item of interest when entering a room
         //Update the camera to keep the protagonist in the centre of the screen but *0.05 so it doesnt snap to them
         this.x += ((gameObject.getX() - x) - windowWidth/2) * 0.05f;
         this.y += ((gameObject.getY() - y) - windowHeight/2) * 0.05f;
--- src/model/KeyInput.java
+++ src/model/KeyInput.java
@@ -14,6 +14,7 @@ import java.util.HashMap;
 
 public class KeyInput{
 
+    //TODO: Can put this in the handler instead of protagonist
     private Scene scene;
     public HashMap<KeyCode, Boolean> keyBinds = new HashMap<>();
 
