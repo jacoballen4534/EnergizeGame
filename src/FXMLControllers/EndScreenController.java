5082013a68d55e382b787189634ee7d088bf76a0
Matteas-Eden <mrmatteden@gmail.com>
2019-04-27 23:04:18 +1200

    Added score calculation to end screen

--- src/FXMLControllers/EndScreenController.java
+++ src/FXMLControllers/EndScreenController.java
@@ -3,6 +3,7 @@ package FXMLControllers;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -12,12 +13,34 @@ import java.util.ResourceBundle;
 
 public class EndScreenController implements Initializable {
 
+    private static final int ITEM_MULTIPLIER = 100;
+    private static final int ENEMY_MULTIPLIER = 200;
+    private static final int TIME_MULTIPLIER = 1;
+    private static final int VICTORY_BONUS = 700;
+
+    public static boolean gameVictory;
+    private static int itemScore;
+    private static int enemyScore;
+    private static int timeScore;
+    private static int victoryScore;
+    private static int finalScore;
+
     @FXML
     private AnchorPane endScreenPane;
 
+    @FXML private Label itemsCollectedLabel;
+    @FXML private Label enemiesKilledLabel;
+    @FXML private Label timeTakenLabel;
+    @FXML private Label victoryBonusLabel;
+    @FXML private Label finalScoreLabel;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        itemsCollectedLabel.setText(Integer.toString(itemScore));
+        enemiesKilledLabel.setText(Integer.toString(enemyScore));
+        timeTakenLabel.setText(Integer.toString(timeScore));
+        victoryBonusLabel.setText(Integer.toString(victoryScore));
+        finalScoreLabel.setText(Integer.toString(finalScore));
     }
 
     @FXML public void titleButtonPressed() throws IOException {
@@ -28,5 +51,14 @@ public class EndScreenController implements Initializable {
     private void ChangeStageName(String newStageName) {
         Stage stage = (Stage) endScreenPane.getScene().getWindow();
         stage.setTitle(newStageName);
+    }
+
+    public static void SetScore(boolean victory, int numItems, int numEnemies, int time){
+        gameVictory = victory;
+        itemScore = numItems * ITEM_MULTIPLIER;
+        enemyScore = numEnemies * ENEMY_MULTIPLIER;
+        timeScore = time * TIME_MULTIPLIER; //Time in seconds
+        victoryScore = (victory) ? VICTORY_BONUS:0;
+        finalScore = itemScore + enemyScore + timeScore + victoryScore;
     }
 }
